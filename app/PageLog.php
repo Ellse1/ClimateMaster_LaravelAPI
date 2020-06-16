304d5aa8020c91903d59d721a9c9e357b488bf5e
Elias Singer <elias.singer@online.de>
2020-06-17 00:18:46 +0200

    return page logs dates

--- app/PageLog.php
+++ app/PageLog.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class PageLog extends Model
 {
-    //
+    public function getCreatedAtAttribute($value)
+    {
+        return \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('Y.m.d');
+    }
+    public function getUpdatedAtAttribute($value){
+        return \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('Y.m.d');
+    }
 }
