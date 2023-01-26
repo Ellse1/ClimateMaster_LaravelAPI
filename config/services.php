a0aed6d7fd5ada9b652565964b7c7e2f14da2405
Elias Singer <elias.singer@online.de>
2023-01-26 13:42:25 +0100

    Update services.php -> Mailgun secret deleted

--- config/services.php
+++ config/services.php
@@ -15,9 +15,6 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT'),
     ],
 
     'postmark' => [
